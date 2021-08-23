af32dfa9780f6871fc0a5d5aeec5709a867c2e7f
Cristhian9507 <84578335+Cristhian9507@users.noreply.github.com>
2021-08-23 16:22:24 -0500

    [JUR-44] se organiza el crud de polizas y se adelanta en relaciones que se pidieron en el camino

---
+++ src/views/Policies/AddPolicy.vue
@@ -0,0 +1,269 @@
+<template>
+  <b-container fluid>
+    <b-row>
+      <b-col md="12">
+        <ValidationObserver ref="form" v-slot="{ handleSubmit }">
+        <form class="mt-4" novalidate @submit.prevent="handleSubmit(onSubmit)">
+        <b-row>
+          <b-col lg="12">
+            <iq-card>
+              <template v-slot:headerTitle>
+                <h4 class="card-title">Creación de Póliza</h4>
+              </template>
+              <template v-slot:body>
+                <div class="new-poliza-info">
+                  <b-row>
+                    <b-form-group class="col-md-6" label="Aseguradora*" label-for="pol_ase_id">
+                      <ValidationProvider name="Aseguradora" rules="required" v-slot="{ errors }">
+                        <v-select v-model="poliza.pol_ase_id" :options="aseguradoraOptions" :reduce="label => label.code" label="label" id="pol_ase_id" :class="(errors.length > 0 ? ' is-invalid' : '')">
+                          <span slot="no-options">No hay aseguradoras.</span>
+                        </v-select>
+                        <div class="invalid-feedback">
+                          <span>Debe de seleccionar una opción</span>
+                        </div>
+                      </ValidationProvider>
+                    </b-form-group>
+                    <b-form-group class="col-md-6" label="Clinica a quien pertenece la póliza*" label-for="usr_role_id">
+                      <ValidationProvider name="Clínica" rules="required" v-slot="{ errors }">
+                        <v-select v-model="poliza.pol_cli_id" :options="clinicaOptions" :reduce="label => label.code" id="pol_ase_id" :class="(errors.length > 0 ? ' is-invalid' : '')">
+                          <span slot="no-options">No hay Pabellones.</span>
+                        </v-select>
+                        <div class="invalid-feedback">
+                          <span>Debe de seleccionar una opción</span>
+                        </div>
+                      </ValidationProvider>
+                    </b-form-group>
+                    <b-form-group class="col-md-6" label="Número de poliza*" label-for="pol_total_asegurado">
+                      <ValidationProvider name="Número de poliza" rules="required" v-slot="{ errors }">
+                        <b-form-input v-model="poliza.pol_numero" type="text" placeholder="9387183671" :class="(errors.length > 0 ? ' is-invalid' : '')"></b-form-input>
+                        <div class="invalid-feedback">
+                          <span>Por favor verifique la información</span>
+                        </div>
+                      </ValidationProvider>
+                    </b-form-group>
+                    <b-form-group class="col-md-6" label="Fecha inicio de vigencia de poliza*" label-for="pol_fecha_inicio">
+                      <ValidationProvider name="Fecha inicio de vigencia de poliza" rules="required" v-slot="{ errors }">
+                        <b-form-input v-model="poliza.pol_fecha_inicio" type="date" :class="(errors.length > 0 ? ' is-invalid' : '')"></b-form-input>
+                        <div class="invalid-feedback">
+                          <span>Por favor verifique la información</span>
+                        </div>
+                      </ValidationProvider>
+                    </b-form-group>
+                    <b-form-group class="col-md-6" label="Fecha fin de vigencia de poliza*" label-for="pol_fecha_fin">
+                      <ValidationProvider name="Fecha inicio de vigencia de poliza" rules="required" v-slot="{ errors }">
+                        <b-form-input v-model="poliza.pol_fecha_fin" type="date" :class="(errors.length > 0 ? ' is-invalid' : '')"></b-form-input>
+                        <div class="invalid-feedback">
+                          <span>Por favor verifique la información</span>
+                        </div>
+                      </ValidationProvider>
+                    </b-form-group>
+                    <b-form-group class="col-md-6" label="Total asegurado*" label-for="pol_total_asegurado">
+                      <ValidationProvider name="Total Asegurado" rules="required" v-slot="{ errors }">
+                        <b-form-input v-model="poliza.pol_total_asegurado" type="number" placeholder="EJ: 10000000" :class="(errors.length > 0 ? ' is-invalid' : '')"></b-form-input>
+                        <div class="invalid-feedback">
+                          <span>Por favor verifique la información</span>
+                        </div>
+                      </ValidationProvider>
+                    </b-form-group>
+                    <b-form-group class="col-md-6" label="Cobertura actual*" label-for="pol_cobertura_actual">
+                      <ValidationProvider name="Cobertual Actual" rules="required" v-slot="{ errors }">
+                        <b-form-input v-model="poliza.pol_cobertura_actual" type="number" placeholder="EJ: 5000000" :class="(errors.length > 0 ? ' is-invalid' : '')"></b-form-input>
+                        <div class="invalid-feedback">
+                          <span>Por favor verifique la información</span>
+                        </div>
+                      </ValidationProvider>
+                    </b-form-group>
+                    <b-form-group class="col-md-6" label="Deducible(%)" label-for="pol_deducible">
+                      <ValidationProvider name="Deducible" rules="max_value: 100" v-slot="{ errors }">
+                        <div class="input-group">
+                          <b-form-input v-model="poliza.pol_deducible" type="number" placeholder="EJ: 10" :class="(errors.length > 0 ? ' is-invalid' : '')"></b-form-input>
+                          <div class="input-group-prepend">
+                            <span class="input-group-text" id="inputGroupPrepend">%</span>
+                          </div>
+                          <div class="invalid-feedback">
+                            <span>El deducible no puede ser mayor a 100%</span>
+                          </div>
+                        </div>
+                      </ValidationProvider>
+                    </b-form-group>
+                    <b-form-group class="col-md-6" label="Deducible Mínimo" label-for="pol_deducible_minimo">
+                      <b-form-input v-model="poliza.pol_deducible_minimo" type="number" placeholder="EJ: 100'000.000" ></b-form-input>
+                    </b-form-group>
+                    <b-form-group class="col-md-6" label="Provisiones Constituidas*" label-for="pol_provisiones">
+                      <ValidationProvider name="Proivisiones Constituidas" rules="required" v-slot="{ errors }">
+                        <b-form-input v-model="poliza.pol_provisiones" type="number" placeholder="EJ: 5000000" :class="(errors.length > 0 ? ' is-invalid' : '')"></b-form-input>
+                        <div class="invalid-feedback">
+                          <span>Por favor verifique la información</span>
+                        </div>
+                      </ValidationProvider>
+                    </b-form-group>
+                    <b-form-group class="col-md-6" ref="addProcess" label-for="exampleFormControlFile1" label="Subir documento de póliza">
+                      <b-form-file id="input-file-import" name="file_import" ref="import_file" @change="onFileChange"></b-form-file>
+                    </b-form-group>
+                    <b-form-group class="col-md-6" label="Estado de la poliza" label-for="pol_estado">
+                      <ValidationProvider name="Estado de la poliza" rules="required" v-slot="{ errors }">
+                        <template v-for="(item,index) in estadoPoliza">
+                          <b-form-radio inline v-model="poliza.pol_estado" :name="item.name" :key="index" :value="item.value" :disabled="item.disabled" :class="(errors.length > 0 ? ' is-invalid' : '')">{{ item.label }}</b-form-radio>
+                        </template>
+                        <div class="invalid-feedback">
+                          <span>{{ errors[0] }}</span>
+                        </div>
+                      </ValidationProvider>
+                    </b-form-group>
+                  </b-row>
+                <b-button variant="primary" type="submit" :class="estadoBoton">{{ textoBoton }}</b-button>
+              </div>
+              </template>
+            </iq-card>
+          </b-col>
+        </b-row>
+        </form>
+        </ValidationObserver>
+      </b-col>
+    </b-row>
+  </b-container>
+
+</template>
+<script>
+import { xray } from '../../config/pluginInit'
+import Vue from 'vue'
+import axios from 'axios'
+import auth from '@/logic/auth'
+
+export default {
+  name: 'AddUser',
+  mounted () {
+    xray.index()
+    this.fetchClinicaOptions()
+    this.fetchAseguradoraOptions()
+  },
+  data () {
+    return {
+      import_file: '',
+      estadoBoton: '',
+      textoBoton: 'Crear Poliza',
+      poliza: {
+        pol_ase_id: '',
+        pol_cli_id: '',
+        pol_numero: '',
+        pol_fecha_inicio: '',
+        pol_fecha_fin: '',
+        pol_total_asegurado: '',
+        pol_cobertura_actual: '',
+        pol_deducible: '',
+        pol_deducible_minimo: '',
+        pol_provisiones: '',
+        pol_url_pdf: '',
+        pol_estado: 1,
+        pol_user_id: ''
+      },
+      aseguradoraOptions: [],
+      clinicaOptions: [],
+      estadoPoliza: [
+        {
+          name: 'activa',
+          label: 'Activa',
+          value: 1,
+          disabled: false
+        },
+        {
+          name: 'inactiva',
+          label: 'Inactiva',
+          value: 0,
+          disabled: false
+        }
+      ],
+      users: [],
+      intentos: 0,
+      errores: []
+    }
+  },
+  computed: {
+    userLogged () {
+      return JSON.parse(auth.getUserLogged())
+    }
+  },
+  methods: {
+    onFileChange (e) {
+      this.import_file = e.target.files[0]
+    },
+    fetchClinicaOptions () {
+      axios.get('/clinicas/obtener-clinicas').then(response => {
+        this.clinicaOptions = response.data.clinicas
+        if (this.clinicaOptions[0] !== undefined) {
+          this.intentos = 0
+          this.errores = {}
+        }
+      })
+        .catch((err) => {
+          this.errores = err
+          if (this.intentos < 2) {
+            this.fetchClinicaOptions()
+            this.intentos++
+          }
+        })
+    },
+    fetchAseguradoraOptions () {
+      axios.get('/aseguradoras/fetch-aseguradoras').then(response => {
+        this.aseguradoraOptions = response.data.aseguradoras
+        if (this.aseguradoraOptions[0] !== undefined) {
+          this.intentos = 0
+          this.errores = {}
+        }
+      })
+        .catch((err) => {
+          this.errores = err
+          if (this.intentos < 2) {
+            this.fetchAseguradoraOptions()
+            this.intentos++
+          }
+        })
+    },
+    onSubmit () {
+      this.estadoBoton = 'disabled'
+      this.addPoliza()
+    },
+    addPoliza () {
+      if (this.userLogged.usr_id != null && this.userLogged.usr_id !== '') {
+        this.poliza.pol_user_id = this.userLogged.usr_id
+        const toke = localStorage.getItem('access_token')
+        let formData = new FormData()
+        formData.append('import_file', this.import_file)
+        formData.append('pol_ase_id', this.poliza.pol_ase_id)
+        formData.append('pol_cli_id', this.poliza.pol_cli_id)
+        formData.append('pol_numero', this.poliza.pol_numero)
+        formData.append('pol_fecha_inicio', this.poliza.pol_fecha_inicio)
+        formData.append('pol_fecha_fin', this.poliza.pol_fecha_fin)
+        formData.append('pol_total_asegurado', this.poliza.pol_total_asegurado)
+        formData.append('pol_cobertura_actual', this.poliza.pol_cobertura_actual)
+        formData.append('pol_deducible', this.poliza.pol_deducible)
+        formData.append('pol_deducible_minimo', this.poliza.pol_deducible_minimo)
+        formData.append('pol_provisiones', this.poliza.pol_provisiones)
+        formData.append('pol_estado', this.poliza.pol_estado)
+        formData.append('pol_user_id', this.poliza.pol_user_id)
+        this.textoBoton = 'Creando Poliza...'
+        axios.post('/policy/registrar-poliza', formData, { headers: { 'Authorization': `Bearer ${toke}` } }).then(res => {
+          this.textoBoton = 'Crear Poliza'
+          this.estadoBoton = ''
+          if (res.data.status_code === 200) {
+            Vue.swal(res.data.message)
+            var editar = false
+            this.$router.push({ path: `/policies/policy-show/${res.data.pol_id}/${editar}` })
+          } else if (res.data.status_code === 401) {
+            Vue.swal(res.data.message)
+          } else {
+            Vue.swal('Datos no validos. ' + res.data.message)
+          }
+        })
+          .catch((err) => {
+            this.textoBoton = 'Crear Poliza'
+            this.estadoBoton = ''
+            Vue.swal('Algo salio mal ' + err)
+          })
+      } else {
+        Vue.swal('Por favor cierra sesión y vuelve a ingresar')
+      }
+    }
+  }
+}
+</script>
---
+++ src/views/Policies/PoliciesArchived.vue
@@ -0,0 +1,257 @@
+<template>
+  <b-container fluid>
+    <!-- User Interface controls -->
+    <b-row>
+      <b-col lg="12">
+        <iq-card>
+          <template v-slot:headerTitle>
+            <h4 class="card-title">Polizas Archivadas</h4>
+          </template>
+          <template v-slot:headerAction>
+            <b-button variant="primary" @click="descargarInforme" :class="estadoBotonDescargarInforme">{{ botonDescargarInforme }}</b-button>
+          </template>
+          <template v-slot:body>
+            <b-row>
+              <b-col sm="4" md="3" class="my-1">
+                <b-form-group
+                  label="Por página"
+                  label-for="per-page-select"
+                  label-cols-sm="5"
+                  label-cols-md="4"
+                  label-cols-lg="5"
+                  label-align-sm="left"
+                  label-size="sm"
+                  class="mb-0"
+                >
+                  <b-form-select
+                    id="per-page-select"
+                    v-model="perPage"
+                    :options="pageOptions"
+                    size="sm"
+                    class="w-50"
+                  ></b-form-select>
+                </b-form-group>
+              </b-col>
+
+              <b-col sm="8" md="9" class="my-1">
+                <b-form-group
+                  label="Buscar"
+                  label-for="filter-input"
+                  label-cols-sm="3"
+                  label-align-sm="right"
+                  label-size="sm"
+                  class="mb-0"
+                >
+                  <b-input-group size="sm">
+                    <b-form-input
+                      id="filter-input"
+                      v-model="filter"
+                      type="search"
+                      placeholder="Escribe para buscar"
+                    ></b-form-input>
+
+                    <b-input-group-append>
+                      <b-button :disabled="!filter" @click="filter = ''">Limpiar</b-button>
+                    </b-input-group-append>
+                  </b-input-group>
+                </b-form-group>
+              </b-col>
+            </b-row>
+            <!-- Main table element -->
+            <b-table
+              :items="policies_archived"
+              :fields="fields"
+              :current-page="currentPage"
+              :per-page="perPage"
+              :filter="filter"
+              :filter-included-fields="filterOn"
+              :sort-by.sync="sortBy"
+              :sort-desc.sync="sortDesc"
+              :sort-direction="sortDirection"
+              stacked="md"
+              show-empty
+              small
+              @filtered="onFiltered"
+            >
+              <template #cell(name)="row">
+                {{ row.value.first }} {{ row.value.last }}
+              </template>
+
+              <template #cell(actions)="row">
+                <b-button variant="primary" @click="verDetalle(row.item.pol_id)">Abrir</b-button>
+              </template>
+            </b-table>
+            <b-row>
+              <b-col sm="4" md="3" class="my-1 text-righ">
+                <b-pagination
+                  v-model="currentPage"
+                  :total-rows="totalRows"
+                  :per-page="perPage"
+                  align="fill"
+                  size="sm"
+                  class="my-0"
+                ></b-pagination>
+              </b-col>
+            </b-row>
+          </template>
+        </iq-card>
+      </b-col>
+    </b-row>
+  </b-container>
+</template>
+<script>
+import auth from '@/logic/auth'
+import { xray } from '../../config/pluginInit'
+import Vue from 'vue'
+import axios from 'axios'
+import moment from 'moment'
+const FileDownload = require('js-file-download')
+
+export default {
+  data () {
+    return {
+      numeroPoliza: null,
+      botonDescargarInforme: 'Descargar Informe',
+      estadoBotonDescargarInforme: 'disabled',
+      botonGuardarModal: '',
+      textoGuardarAfectacion: 'Guardar',
+      aseguradora_id: '',
+      user_id: '',
+      policies_archived: [],
+      afectacionPoliza: {
+        pol_affe_pol_id: '',
+        pol_affe_prore_id: '',
+        pol_affe_valor_bruto: '',
+        pol_affe_valor_deducible: '',
+        pol_affe_valor_neto: '',
+        pol_affe_fecha: '',
+        pol_affe_user_id: ''
+      },
+      error: {},
+      aseguradoraOptions: [],
+      processOptions: [],
+      polizaOptions: [],
+      fields: [
+        { key: 'pol_numero', label: 'N° Pol.', sortable: true, sortDirection: 'desc', class: 'text-left' },
+        { key: 'clinica.cli_name', label: 'Clinica', sortable: true, sortDirection: 'desc', class: 'text-left' },
+        { key: 'aseguradora.ase_name', label: 'Aseguradora', sortable: true, class: 'text-left' },
+        {
+          key: 'pol_total_asegurado',
+          label: 'Total Asegurado',
+          formatter: (value, key, item) => {
+            return this.formatPrice(value, key, item)
+          },
+          class: 'text-center'
+        },
+        {
+          key: 'pol_cobertura_actual',
+          label: 'Cobertura Actual',
+          formatter: (value, key, item) => {
+            return this.formatPrice(value, key, item)
+          },
+          class: 'text-center'
+        },
+        {
+          key: 'pol_deducible',
+          label: 'Deducible',
+          formatter: (value, key, item) => {
+            if (value !== '' && value != null) {
+              return value + '%'
+            } else {
+              return '0%'
+            }
+          },
+          class: 'text-center'
+        },
+        { key: 'actions', label: 'Acciones', class: 'text-center' }
+      ],
+      totalRows: 1,
+      currentPage: 1,
+      perPage: 5,
+      pageOptions: [5, 10, 15, { value: 100, text: 'Muchas' }],
+      sortBy: '',
+      sortDesc: false,
+      estado_elegido: 'todos',
+      sortDirection: 'asc',
+      filter: null,
+      filterOn: [],
+      infoModal: {
+        id: 'info-modal',
+        title: '',
+        content: ''
+      },
+      links: {},
+      intentos: 0,
+      errores: {},
+      errors: []
+    }
+  },
+  computed: {
+    userLogged () {
+      return JSON.parse(auth.getUserLogged())
+    },
+    sortOptions () {
+      // Create an options list from our fields
+      return this.fields
+        .filter(f => f.sortable)
+        .map(f => {
+          return { text: f.label, value: f.key }
+        })
+    }
+  },
+  mounted () {
+    xray.index()
+    this.fetchPolicies()
+  },
+  methods: {
+    fetchPolicies () {
+      axios.get('/policy/obtener-polizas-archivadas').then(response => {
+        this.policies_archived = response.data.policies_archived
+        if (this.policies_archived[0] !== undefined) {
+          this.intentos = 0
+          this.errores = {}
+        }
+      })
+        .catch((err) => {
+          this.errores = err
+          if (this.intentos < 2) {
+            this.fetchPolicies()
+            this.intentos++
+          }
+        })
+    },
+    verDetalle (polId) {
+      var editar = false
+      this.$router.push({ path: `/policies/policy-show/${polId}/${editar}` })
+    },
+    onFiltered (filteredItems) {
+      // Trigger pagination to update the number of buttons/pages due to filtering
+      this.totalRows = filteredItems.length
+      this.currentPage = 1
+    },
+    descargarInforme () {
+      this.botonDescargarInforme = 'Descargando informe...'
+      this.estadoBotonDescargarInforme = 'disabled'
+      axios({
+        url: '/process/exportReport',
+        method: 'GET',
+        responseType: 'blob'
+      }).then((response) => {
+        this.botonDescargarInforme = 'Descargar informe'
+        this.estadoBotonDescargarInforme = ''
+        var fechaHora = moment().format('YYYY-MM-DD hh:mm:ss')
+        FileDownload(response.data, 'report-process-activos-' + fechaHora + '.xlsx')
+      })
+        .catch((err) => {
+          this.botonDescargarInforme = 'Descargar informe'
+          this.estadoBotonDescargarInforme = ''
+          Vue.swal('Ups, ocurrió un error ' + err)
+        })
+    },
+    formatPrice (value) {
+      let val = (value / 1).toFixed(0).replace('.', ',')
+      return '$ ' + val.toString().replace(/\B(?=(\d{3})+(?!\d))/g, '.')
+    }
+  }
+}
+</script>
