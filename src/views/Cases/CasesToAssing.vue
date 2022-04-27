8904b5f16d36cf2aa2a81e259a7448c6099b737f
josueco123 <45926578+josueco123@users.noreply.github.com>
2022-04-27 15:05:29 -0500

    [JUR-152]-feature-crear-listado-de-casos-por-asignar

---
+++ src/views/Cases/CasesToAssing.vue
@@ -0,0 +1,239 @@
+<template>
+  <b-container fluid>
+       <!-- MODAL DE EDITAR CASO -->
+        <div>
+          <b-modal id="modal-editar-caso" size="lg" title="Editar Caso" hide-footer>
+            <ValidationObserver ref="form" v-slot="{ handleSubmit }">
+            <form ref="form" @submit.prevent="handleSubmit(onSubmit)">
+               <b-row class="justify-content-center text-center align-items-center">
+                        <b-col lg="6">
+                        <b-form-group  label="Titulo de la Solicitud*" label-for="case_title">
+                            <b-form-input v-model="caso.caso_titulo" type="text" :required="true" ></b-form-input>
+                        </b-form-group>
+                        <b-form-group  label="Descripción*" label-for="textarea-decription">
+                            <b-form-textarea id="textarea-decription" v-model="caso.caso_descripcion" :required="true" ></b-form-textarea>
+                        </b-form-group>
+                        <b-form-group >
+                            <b-button variant="primary" type="submit"  >Editar Caso</b-button>
+                        </b-form-group>
+                        </b-col>
+                    </b-row>
+            </form>
+            </ValidationObserver>
+          </b-modal>
+        </div>
+        <!-- FIN DE MODAL-->
+    <b-row>
+      <b-col lg="12">
+        <iq-card>
+          <template v-slot:headerTitle>
+            <h4 class="card-title">Casos Por Asignar</h4>
+          </template>
+          <template v-slot:body>
+          <b-row>
+            <b-col sm="3" md="3" class="my-1">
+              <b-form-group
+                label="Por página"
+                label-for="per-page-select"
+                label-cols-sm="5"
+                label-cols-md="4"
+                label-cols-lg="5"
+                label-align-sm="left"
+                label-size="sm"
+                class="mb-0"
+              >
+                <b-form-select
+                  id="per-page-select"
+                  v-model="perPage"
+                  :options="pageOptions"
+                  size="sm"
+                  class="w-80"
+                ></b-form-select>
+              </b-form-group>
+            </b-col>
+
+            <b-col sm="7" md="7" class="my-1">
+              <b-form-group
+                label="Buscar"
+                label-for="filter-input"
+                label-cols-sm="3"
+                label-align-sm="right"
+                label-size="sm"
+                class="mb-0"
+              >
+                <b-input-group size="sm">
+                  <b-form-input
+                    id="filter-input"
+                    v-model="filter"
+                    type="search"
+                    placeholder="Escribe para buscar"
+                  ></b-form-input>
+
+                  <b-input-group-append>
+                    <b-button :disabled="!filter" @click="filter = ''">Limpiar</b-button>
+                  </b-input-group-append>
+                </b-input-group>
+              </b-form-group>
+            </b-col>
+          </b-row>
+
+          <!-- Main table element -->
+          <b-table
+            :items="casos"
+            :fields="fields"
+            :current-page="currentPage"
+            :per-page="perPage"
+            :filter="filter"
+            :filter-included-fields="filterOn"
+            :sort-by.sync="sortBy"
+            :sort-desc.sync="sortDesc"
+            :sort-direction="sortDirection"
+            stacked="md"
+            show-empty
+            small
+            @filtered="onFiltered"
+          >
+        <template #cell(name)="row">
+          {{ row.value.first }} {{ row.value.last }}
+        </template>
+        <template #cell(actions)="row">
+           <b-dropdown variant="primary" text="Acciones">
+              <b-dropdown-item @click="verCaso(row.item.caso_id)">
+                Ver
+              </b-dropdown-item>
+              <b-dropdown-item @click="editarCaso(row.item)">
+                Editar
+              </b-dropdown-item>
+              <b-dropdown-item v-if="user_profile == 1" @click="eliminarCaso(row.item.caso_id)" >
+                Eliminar
+              </b-dropdown-item>
+          </b-dropdown>
+        </template>
+
+      </b-table>
+      <b-row>
+        <b-col sm="4" md="3" class="my-1 text-righ">
+          <b-pagination
+            v-model="currentPage"
+            :total-rows="totalRows"
+            :per-page="perPage"
+            align="fill"
+            size="sm"
+            class="my-0"
+          ></b-pagination>
+        </b-col>
+      </b-row>
+          </template>
+        </iq-card>
+      </b-col>
+    </b-row>
+  </b-container>
+</template>
+
+<script>
+import axios from 'axios'
+import auth from '@/logic/auth'
+import Vue from 'vue'
+import Swal from 'sweetalert2/dist/sweetalert2.js'
+export default {
+  name: 'CasesToAssing',
+  data () {
+    return {
+      casos: [],
+      caso: {},
+      estado: 'd-none',
+      bRowLast: {},
+      fields: [
+        { label: 'Título', key: 'caso_titulo', class: 'text-left' },
+        { label: 'Descripción', key: 'caso_descripcion', class: 'text-left' },
+        { label: 'Estado', key: 'estado', class: 'text-left' },
+        { label: 'Fecha de Apertura', key: 'caso_fecha_apertura', class: 'text-left' },
+        { label: 'Acciones', key: 'actions', class: 'text-center' }
+      ],
+      totalRows: 1,
+      currentPage: 1,
+      perPage: 5,
+      pageOptions: [5, 10, 15, { value: 100, text: 'Muchas' }],
+      sortBy: '',
+      sortDesc: false,
+      sortDirection: 'asc',
+      filter: null,
+      filterOn: [],
+      user_profile: null
+    }
+  },
+  computed: {
+    sortOptions () {
+      // Create an options list from our fields
+      return this.fields
+        .filter(f => f.sortable)
+        .map(f => {
+          return { text: f.label, value: f.key }
+        })
+    },
+    userLogged () {
+      return JSON.parse(auth.getUserLogged())
+    }
+  },
+  mounted () {
+    this.getCasosToAssing()
+  },
+  methods: {
+    onFiltered (filteredItems) {
+      // Trigger pagination to update the number of buttons/pages due to filtering
+      this.totalRows = filteredItems.length
+      this.currentPage = 1
+    },
+    getCasosToAssing () {
+      axios.get('/casos/por-asignar').then(response => {
+        this.casos = response.data.casos
+        this.totalRows = this.casos.length
+        this.user_profile = this.userLogged.user_profile
+      })
+    },
+    verCaso (casoId) {
+      this.$router.push({ path: `/cases/cases-show/${casoId}` })
+    },
+    editarCaso (caso) {
+      this.caso = caso
+      this.$bvModal.show('modal-editar-caso')
+    },
+    onSubmit () {
+      this.actualizarCaso()
+    },
+    actualizarCaso () {
+      axios.post('/casos/update/' + this.caso.caso_id, this.caso).then(res => {
+        if (res.data.status_code === 200) {
+          Vue.swal(res.data.message)
+          this.getCasosToAssing()
+        } else {
+          Vue.swal(res.data.message)
+        }
+      })
+    },
+    eliminarCaso (casoId) {
+      Swal.fire({
+        icon: 'warning',
+        title: '¿Estás seguro?',
+        text: '¿Deseas eliminar este Caso?',
+        showCancelButton: true,
+        confirmButtonText: 'Eliminar'
+      }).then((result) => {
+        if (result.isConfirmed) {
+          axios.get('/casos/delete/' + casoId).then(res => {
+            if (res.status === 200) {
+              Vue.swal(res.data.message)
+              this.getCasosToAssing()
+            } else {
+              Vue.swal(res.data.message)
+            }
+          })
+            .catch((err) => {
+              Vue.swal('Ups sucedió un error tratando de consulta la información. ' + err)
+            })
+        }
+      })
+    }
+  }
+}
+</script>
