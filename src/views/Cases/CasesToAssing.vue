5f911e61280282e070ad97ede32cff7974bfe347
josueco123 <45926578+josueco123@users.noreply.github.com>
2022-05-03 11:33:47 -0500

    [JUR-171]-feature-implementar-logica-para-asignar-casos

--- src/views/Cases/CasesToAssing.vue
+++ src/views/Cases/CasesToAssing.vue
@@ -1,28 +1,119 @@
 <template>
   <b-container fluid>
-       <!-- MODAL DE EDITAR CASO -->
-        <div>
-          <b-modal id="modal-editar-caso" size="lg" title="Editar Caso" hide-footer>
-            <ValidationObserver ref="form" v-slot="{ handleSubmit }">
-            <form ref="form" @submit.prevent="handleSubmit(onSubmit)">
-               <b-row class="justify-content-center text-center align-items-center">
-                        <b-col lg="6">
-                        <b-form-group  label="Titulo de la Solicitud*" label-for="case_title">
-                            <b-form-input v-model="caso.caso_titulo" type="text" :required="true" ></b-form-input>
-                        </b-form-group>
-                        <b-form-group  label="Descripción*" label-for="textarea-decription">
-                            <b-form-textarea id="textarea-decription" v-model="caso.caso_descripcion" :required="true" ></b-form-textarea>
-                        </b-form-group>
-                        <b-form-group >
-                            <b-button variant="primary" type="submit"  >Editar Caso</b-button>
-                        </b-form-group>
-                        </b-col>
-                    </b-row>
-            </form>
-            </ValidationObserver>
-          </b-modal>
-        </div>
-        <!-- FIN DE MODAL-->
+    <!-- MODAL DE EDITAR CASO -->
+    <div>
+      <b-modal id="modal-editar-caso" size="lg" title="Editar Caso" hide-footer>
+        <ValidationObserver ref="form" v-slot="{ handleSubmit }">
+          <form ref="form" @submit.prevent="handleSubmit(onSubmit)">
+            <b-row
+              class="justify-content-center text-center align-items-center"
+            >
+              <b-col lg="6">
+                <b-form-group
+                  label="Titulo de la Solicitud*"
+                  label-for="case_title"
+                >
+                  <b-form-input
+                    v-model="caso.caso_titulo"
+                    type="text"
+                    :required="true"
+                  ></b-form-input>
+                </b-form-group>
+                <b-form-group
+                  label="Descripción*"
+                  label-for="textarea-decription"
+                >
+                  <b-form-textarea
+                    id="textarea-decription"
+                    v-model="caso.caso_descripcion"
+                    :required="true"
+                  ></b-form-textarea>
+                </b-form-group>
+                <b-form-group>
+                  <b-button variant="primary" type="submit"
+                    >Editar Caso</b-button
+                  >
+                </b-form-group>
+              </b-col>
+            </b-row>
+          </form>
+        </ValidationObserver>
+      </b-modal>
+    </div>
+    <!-- FIN DE MODAL-->
+    <!-- MODAL DE ASIGNAR CASO -->
+    <div>
+      <b-modal
+        id="modal-asignar-caso"
+        size="lg"
+        title="Asignar Caso"
+        hide-footer
+      >
+        <ValidationObserver ref="form-2" v-slot="{ handleSubmit }">
+          <form ref="form-2" @submit.prevent="handleSubmit(SaveAsignacionCaso)">
+            <b-row>
+              <b-col lg="10" class="pagina-detalle-proceso">
+                <p>
+                  <strong>{{ caso.caso_titulo }}</strong>
+                </p>
+                <p>{{ caso.caso_descripcion }}</p>
+                <b-form-group label="Abogado*" label-for="profesional_id">
+                  <v-select
+                    v-model="asginarData.profesional_id"
+                    :options="profesionalesOptions"
+                    :reduce="(label) => label.code"
+                    label="label"
+                    id="profesional_id"
+                  >
+                    <span slot="no-options">No hay Abogados.</span>
+                  </v-select>
+                </b-form-group>
+                <b-row>
+                  <b-form-group
+                    class="col-md-6"
+                    label="Actividad*"
+                    label-for="act_id"
+                  >
+                    <v-select
+                      v-model="actividad_id"
+                      :options="actividadesOptions"
+                      :reduce="(label) => label.code"
+                      label="label"
+                      id="act_id"
+                      @input="getSubactividades"
+                    >
+                      <span slot="no-options">No hay Actividades.</span>
+                    </v-select>
+                  </b-form-group>
+                  <b-form-group
+                    class="col-md-6"
+                    label="Subactividad*"
+                    label-for="subact_id"
+                  >
+                    <v-select
+                      v-model="asginarData.subactividad_id"
+                      :options="subactividadesOptions"
+                      :reduce="(label) => label.code"
+                      label="label"
+                      id="subact_id"
+                    >
+                      <span slot="no-options">No hay Subctividades.</span>
+                    </v-select>
+                  </b-form-group>
+                </b-row>
+                <b-form-group>
+                  <b-button variant="primary" type="submit"
+                    >Asignar Caso</b-button
+                  >
+                </b-form-group>
+              </b-col>
+            </b-row>
+          </form>
+        </ValidationObserver>
+      </b-modal>
+    </div>
+    <!-- FIN DE MODAL-->
+
     <b-row>
       <b-col lg="12">
         <iq-card>
@@ -30,102 +121,106 @@
             <h4 class="card-title">Casos Por Asignar</h4>
           </template>
           <template v-slot:body>
-          <b-row>
-            <b-col sm="3" md="3" class="my-1">
-              <b-form-group
-                label="Por página"
-                label-for="per-page-select"
-                label-cols-sm="5"
-                label-cols-md="4"
-                label-cols-lg="5"
-                label-align-sm="left"
-                label-size="sm"
-                class="mb-0"
-              >
-                <b-form-select
-                  id="per-page-select"
-                  v-model="perPage"
-                  :options="pageOptions"
+            <b-row>
+              <b-col sm="3" md="3" class="my-1">
+                <b-form-group
+                  label="Por página"
+                  label-for="per-page-select"
+                  label-cols-sm="5"
+                  label-cols-md="4"
+                  label-cols-lg="5"
+                  label-align-sm="left"
+                  label-size="sm"
+                  class="mb-0"
+                >
+                  <b-form-select
+                    id="per-page-select"
+                    v-model="perPage"
+                    :options="pageOptions"
+                    size="sm"
+                    class="w-80"
+                  ></b-form-select>
+                </b-form-group>
+              </b-col>
+
+              <b-col sm="7" md="7" class="my-1">
+                <b-form-group
+                  label="Buscar"
+                  label-for="filter-input"
+                  label-cols-sm="3"
+                  label-align-sm="right"
+                  label-size="sm"
+                  class="mb-0"
+                >
+                  <b-input-group size="sm">
+                    <b-form-input
+                      id="filter-input"
+                      v-model="filter"
+                      type="search"
+                      placeholder="Escribe para buscar"
+                    ></b-form-input>
+
+                    <b-input-group-append>
+                      <b-button :disabled="!filter" @click="filter = ''"
+                        >Limpiar</b-button
+                      >
+                    </b-input-group-append>
+                  </b-input-group>
+                </b-form-group>
+              </b-col>
+            </b-row>
+
+            <!-- Main table element -->
+            <b-table
+              :items="casos"
+              :fields="fields"
+              :current-page="currentPage"
+              :per-page="perPage"
+              :filter="filter"
+              :filter-included-fields="filterOn"
+              :sort-by.sync="sortBy"
+              :sort-desc.sync="sortDesc"
+              :sort-direction="sortDirection"
+              stacked="md"
+              show-empty
+              small
+              @filtered="onFiltered"
+            >
+              <template #cell(name)="row">
+                {{ row.value.first }} {{ row.value.last }}
+              </template>
+              <template #cell(actions)="row">
+                <b-dropdown variant="primary" text="Acciones">
+                  <b-dropdown-item @click="verCaso(row.item.caso_id)">
+                    Ver
+                  </b-dropdown-item>
+                  <b-dropdown-item @click="editarCaso(row.item)">
+                    Editar
+                  </b-dropdown-item>
+                  <b-dropdown-item @click="asignarCaso(row.item)">
+                    Asignar
+                  </b-dropdown-item>
+                  <b-dropdown-item
+                    v-if="user_profile == 1"
+                    @click="eliminarCaso(row.item.caso_id)"
+                  >
+                    Eliminar
+                  </b-dropdown-item>
+                </b-dropdown>
+              </template>
+            </b-table>
+            <b-row>
+              <b-col sm="4" md="3" class="my-1 text-righ">
+                <b-pagination
+                  v-model="currentPage"
+                  :total-rows="totalRows"
+                  :per-page="perPage"
+                  align="fill"
                   size="sm"
-                  class="w-80"
-                ></b-form-select>
-              </b-form-group>
-            </b-col>
-
-            <b-col sm="7" md="7" class="my-1">
-              <b-form-group
-                label="Buscar"
-                label-for="filter-input"
-                label-cols-sm="3"
-                label-align-sm="right"
-                label-size="sm"
-                class="mb-0"
-              >
-                <b-input-group size="sm">
-                  <b-form-input
-                    id="filter-input"
-                    v-model="filter"
-                    type="search"
-                    placeholder="Escribe para buscar"
-                  ></b-form-input>
-
-                  <b-input-group-append>
-                    <b-button :disabled="!filter" @click="filter = ''">Limpiar</b-button>
-                  </b-input-group-append>
-                </b-input-group>
-              </b-form-group>
-            </b-col>
-          </b-row>
-
-          <!-- Main table element -->
-          <b-table
-            :items="casos"
-            :fields="fields"
-            :current-page="currentPage"
-            :per-page="perPage"
-            :filter="filter"
-            :filter-included-fields="filterOn"
-            :sort-by.sync="sortBy"
-            :sort-desc.sync="sortDesc"
-            :sort-direction="sortDirection"
-            stacked="md"
-            show-empty
-            small
-            @filtered="onFiltered"
-          >
-        <template #cell(name)="row">
-          {{ row.value.first }} {{ row.value.last }}
-        </template>
-        <template #cell(actions)="row">
-           <b-dropdown variant="primary" text="Acciones">
-              <b-dropdown-item @click="verCaso(row.item.caso_id)">
-                Ver
-              </b-dropdown-item>
-              <b-dropdown-item @click="editarCaso(row.item)">
-                Editar
-              </b-dropdown-item>
-              <b-dropdown-item @click="editarCaso(row.item)">
-                Asignar
-              </b-dropdown-item>
-              <b-dropdown-item v-if="user_profile == 1" @click="eliminarCaso(row.item.caso_id)" >
-                Eliminar
-              </b-dropdown-item>
-          </b-dropdown>
-        </template>
-
-      </b-table>
-      <b-row>
-        <b-col sm="4" md="3" class="my-1 text-righ">
-          <b-pagination
-            v-model="currentPage"
-            :total-rows="totalRows"
-            :per-page="perPage"
-            align="fill"
-            size="sm"
-            class="my-0"
-          ></b-pagination>
-        </b-col>
-      </b-row>
+                  class="my-0"
+                ></b-pagination>
+              </b-col>
+            </b-row>
           </template>
         </iq-card>
       </b-col>
@@ -145,12 +240,24 @@ export default {
       casos: [],
       caso: {},
       estado: 'd-none',
+      actividadesOptions: [],
+      subactividadesOptions: [],
+      profesionalesOptions: [],
       bRowLast: {},
+      actividad_id: '',
+      asginarData: {
+        subactividad_id: '',
+        profesional_id: ''
+      },
       fields: [
         { label: 'Título', key: 'caso_titulo', class: 'text-left' },
         { label: 'Descripción', key: 'caso_descripcion', class: 'text-left' },
         { label: 'Estado', key: 'estado', class: 'text-left' },
-        { label: 'Fecha de Apertura', key: 'caso_fecha_apertura', class: 'text-left' },
+        {
+          label: 'Fecha de Apertura',
+          key: 'caso_fecha_apertura',
+          class: 'text-left'
+        },
         { label: 'Acciones', key: 'actions', class: 'text-center' }
       ],
       totalRows: 1,
@@ -169,8 +276,8 @@ export default {
     sortOptions () {
       // Create an options list from our fields
       return this.fields
-        .filter(f => f.sortable)
-        .map(f => {
+        .filter((f) => f.sortable)
+        .map((f) => {
           return { text: f.label, value: f.key }
         })
     },
@@ -188,7 +295,7 @@ export default {
       this.currentPage = 1
     },
     getCasosToAssing () {
-      axios.get('/casos/por-asignar').then(response => {
+      axios.get('/casos/por-asignar').then((response) => {
         this.casos = response.data.casos
         this.totalRows = this.casos.length
         this.user_profile = this.userLogged.user_profile
@@ -201,18 +308,26 @@ export default {
       this.caso = caso
       this.$bvModal.show('modal-editar-caso')
     },
+    asignarCaso (caso) {
+      this.getActividades()
+      this.getProfesionals()
+      this.caso = caso
+      this.$bvModal.show('modal-asignar-caso')
+    },
     onSubmit () {
       this.actualizarCaso()
     },
     actualizarCaso () {
-      axios.post('/casos/update/' + this.caso.caso_id, this.caso).then(res => {
-        if (res.data.status_code === 200) {
-          Vue.swal(res.data.message)
-          this.getCasosToAssing()
-        } else {
-          Vue.swal(res.data.message)
-        }
-      })
+      axios
+        .post('/casos/update/' + this.caso.caso_id, this.caso)
+        .then((res) => {
+          if (res.data.status_code === 200) {
+            Vue.swal(res.data.message)
+            this.getCasosToAssing()
+          } else {
+            Vue.swal(res.data.message)
+          }
+        })
     },
     eliminarCaso (casoId) {
       Swal.fire({
@@ -223,19 +338,53 @@ export default {
         confirmButtonText: 'Eliminar'
       }).then((result) => {
         if (result.isConfirmed) {
-          axios.get('/casos/delete/' + casoId).then(res => {
-            if (res.status === 200) {
-              Vue.swal(res.data.message)
-              this.getCasosToAssing()
-            } else {
-              Vue.swal(res.data.message)
-            }
-          })
+          axios
+            .get('/casos/delete/' + casoId)
+            .then((res) => {
+              if (res.status === 200) {
+                Vue.swal(res.data.message)
+                this.getCasosToAssing()
+              } else {
+                Vue.swal(res.data.message)
+              }
+            })
             .catch((err) => {
-              Vue.swal('Ups sucedió un error tratando de consulta la información. ' + err)
+              Vue.swal(
+                'Ups sucedió un error tratando de consulta la información. ' +
+                  err
+              )
             })
         }
       })
+    },
+    getActividades () {
+      axios.get('/actividades/fetch').then((response) => {
+        this.actividadesOptions = response.data.actividades
+      })
+    },
+    getSubactividades () {
+      axios
+        .get('/subactividades/fetch/' + this.actividad_id)
+        .then((response) => {
+          this.subactividadesOptions = response.data.subactividades
+        })
+    },
+    getProfesionals () {
+      axios.get('/professionals/fetch').then((response) => {
+        this.profesionalesOptions = response.data.professionals
+      })
+    },
+    SaveAsignacionCaso () {
+      axios
+        .post('/casos/asignar/' + this.caso.caso_id, this.asginarData)
+        .then((res) => {
+          if (res.status === 200) {
+            Vue.swal(res.data.message)
+            this.getCasosToAssing()
+          } else {
+            Vue.swal(res.data.message)
+          }
+        })
     }
   }
 }
