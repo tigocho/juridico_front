825d3d563634cf862e712bcb47a9b81a3fddb761
josueco123 <45926578+josueco123@users.noreply.github.com>
2022-05-25 16:13:53 -0500

    [JUR-197]-feature-grafico-de-casos-por-tipo-de-servicio

---
+++ src/views/Cases/components/CasosPorSubactividad.vue
@@ -0,0 +1,117 @@
+<template>
+  <div>
+    <div :id="element"></div>
+    <div v-if="!hasData">
+      <h4 class="col-12 text-center">No hay Casos</h4>
+    </div>
+  </div>
+</template>
+<script>
+import ApexCharts from 'apexcharts'
+
+export default {
+  name: 'GraficaCasosPorSubactividad',
+  props: {
+    element: String,
+    casosSubactividad: Array
+  },
+  data () {
+    return {
+      hasData: false,
+      chartOptions: {
+        series: [
+          {
+            name: 'Num. de Casos',
+            data: []
+          }
+        ],
+        chart: {
+          height: 500,
+          type: 'bar'
+        },
+        colors: ['#089bab'],
+        plotOptions: {
+          bar: {
+            dataLabels: {
+              position: 'top'
+            },
+            columnWidth: '80%',
+            endingShape: 'rounded'
+          }
+        },
+        dataLabels: {
+          enabled: true,
+          position: 'top',
+          style: {
+            colors: ['#089bab']
+          },
+          offsetY: -25
+        },
+        stroke: {
+          width: 2
+        },
+        grid: {
+          row: {
+            colors: ['#fff', '#f2f2f2']
+          }
+        },
+        xaxis: {
+          type: 'category',
+          labels: {
+            rotateAlways: true,
+            minHeight: 150,
+            maxHeight: 1000,
+            trim: true
+          },
+          categories: [],
+          tickPlacement: 'on'
+        },
+        yaxis: {
+          title: {
+            text: 'Número de Casos'
+          }
+        },
+        fill: {
+          type: 'gradient',
+          gradient: {
+            shade: 'light',
+            type: 'horizontal',
+            shadeIntensity: 0.25,
+            gradientToColors: undefined,
+            inverseColors: true,
+            opacityFrom: 0.85,
+            opacityTo: 0.85,
+            stops: [50, 0, 100]
+          }
+        }
+      }
+    }
+  },
+  mounted () {
+    this.crearGrafico()
+  },
+  methods: {
+    crearGrafico () {
+      const selector = '#' + this.element
+      const subactividad = []
+      const cantidad = []
+
+      for (let caso of this.casosSubactividad) {
+        cantidad.push(caso.cantidad)
+        subactividad.push(caso.subactividad)
+      }
+
+      if (this.casosSubactividad.length > 0) this.hasData = true
+
+      this.chartOptions.xaxis.categories = subactividad
+      this.chartOptions.series[0].data = cantidad
+      const chart = new ApexCharts(
+        document.querySelector(selector),
+        this.chartOptions
+      )
+
+      chart.render()
+    }
+  }
+}
+</script>
