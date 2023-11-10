951d6e3c7c4c0656dd434eb40bd165e0e73a0e26
Cristhian Castro <cristhian.castro@ospedale.com.co>
2023-11-10 17:22:11 -0500

    cambios para probar en producción

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
-const webpack = require('webpack')
-const { GenerateSW } = require('workbox-webpack-plugin')
+// const webpack = require('webpack')
+// const { GenerateSW } = require('workbox-webpack-plugin')
 
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
@@ -8,29 +8,29 @@ module.exports = {
 
   transpileDependencies: ['vuetify'],
 
-  chainWebpack: config => {
-    config.performance
-      .maxEntrypointSize(4000000000)
-      .maxAssetSize(4000000000)
-  },
+  // chainWebpack: config => {
+  //   config.performance
+  //     .maxEntrypointSize(4000000000)
+  //     .maxAssetSize(4000000000)
+  // },
 
-  configureWebpack: {
-    plugins: [
-      new webpack.ProvidePlugin({
-        mapboxgl: 'mapbox-gl'
-      }),
-      new GenerateSW({
-        // Configuración del precaching
-        runtimeCaching: [
-          {
-            urlPattern: /\.(?:png|jpg|jpeg|svg)$/,
-            handler: 'CacheFirst'
-          }
-          // Puedes agregar más patrones y configuraciones según tus necesidades
-        ]
-      })
-    ]
-  },
+  // configureWebpack: {
+  //   plugins: [
+  //     new webpack.ProvidePlugin({
+  //       mapboxgl: 'mapbox-gl'
+  //     }),
+  //     new GenerateSW({
+  //       // Configuración del precaching
+  //       runtimeCaching: [
+  //         {
+  //           urlPattern: /\.(?:png|jpg|jpeg|svg)$/,
+  //           handler: 'CacheFirst'
+  //         }
+  //         // Puedes agregar más patrones y configuraciones según tus necesidades
+  //       ]
+  //     })
+  //   ]
+  // },
 
   pluginOptions: {
     i18n: {
